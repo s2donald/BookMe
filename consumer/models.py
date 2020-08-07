0b03a09b49832db1d25ae4c2f73518cdccfd1877
Stephen Donald <s2donald@edu.uwaterloo.ca>
2020-08-06 23:38:32 -0400

    Added some calendar functionalities

--- consumer/models.py
+++ consumer/models.py
@@ -1,27 +1,31 @@
 from django.db import models
 from business.models import Account, Company, Services
 from django.utils import timezone
+
 # Create your models here.
 
 class Bookings(models.Model):
     #The user who booked
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
-    #The service booked
+    #The service booked (also contains the company the service is with)
     service = models.ForeignKey(Services, on_delete=models.CASCADE)
-    #Add the company the service is with
+    #The company which is offering this service
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     #We must add a timeslot for the booking
+    #Has the booking been paid for
     has_paid = models.BooleanField(default=False)
     #We must also create a receipt model to handle the reciepts and link to the booking
     start = models.DateTimeField(default=timezone.now)
     end = models.DateTimeField(default=timezone.now)
-
     #is the service available at this time
     is_avail = models.BooleanField(default=False)
     class Meta:
-        ordering = ('service',)
+        ordering = ('start',)
         verbose_name = 'booking'
         verbose_name_plural = 'bookings'
+
+    def __str__(self):
+        return self.service.name
 
 
 
--- consumer/views.py
+++ consumer/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from business.models import Company
+from .models import Bookings
 # Create your views here.
 
 @login_required
 def bookingScheduleView(request, id, slug):
     company = get_object_or_404(Company, id=id, slug=slug, available=True)
-    return render(request, 'business/booking/bookingCalendar.html', {'company':company})
+    bookings = Bookings.objects.filter(company=company)
+    return render(request, 'business/booking/bookingCalendar.html', {'company':company, 'bookings':bookings})

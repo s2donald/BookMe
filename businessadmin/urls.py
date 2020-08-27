89ed04c1f22297219ca43e900ae62a37e08ae650
Stephen Donald <s2donald@edu.uwaterloo.ca>
2020-08-27 05:25:46 -0400

    adding the main page for business, need to incorporate the business admin soon

--- businessadmin/urls.py
+++ businessadmin/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url, include
 from . import views
 app_name = 'businessadmin'
 urlpatterns = [
+    url(r'^faq/$', views.faqBusinessViews, name='faqbusiness'),
+    url(r'^pricing/$', views.pricingViews, name='pricingBusiness'),
     url(r'^$', views.businessadmin, name='bizadminmain'),
 ]
--- businessadmin/views.py
+++ businessadmin/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def businessadmin(request):
-    return render(request, 'welcome/main.html')
+    return render(request, 'welcome/welcome.html')
+
+def pricingViews(request):
+    return render(request, 'welcome/pricing.html')
+
+def faqBusinessViews(request):
+    return render(request, 'welcome/faq.html')

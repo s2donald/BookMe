a0d806aaffe4a9d26195ef07e6d28fb0893b0040
Stephen Donald <s2donald@uwaterloo.ca>
2020-10-13 15:26:37 -0400

    Changes

--- gibele/hosts.py
+++ gibele/hosts.py
@@ -5,7 +5,7 @@ settings.PARENT_HOST = 'herokuapp.com'
 host_patterns = patterns('',
     # host(r'www', 'gibele.urls',name='ww'),
     host(r'pure-fjord-45840', settings.ROOT_URLCONF, name='www'),
-    host(r'business','businessadmin.urls', name='bizadmin'),
+    host(r'business.pure-fjord-45840.','businessadmin.urls', name='bizadmin'),
     host(r'(?P<slug>[-\w]+)', 'calendarapp.urls', callback='calendarapp.views.get_companyslug',name='bookingurl'),
 
     # host(r'', settings.ROOT_URLCONF, name='www'),

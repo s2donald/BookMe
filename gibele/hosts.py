248c754f0110ae2a8fd18ca3144017af31ad2a92
Stephen Donald <s2donald@uwaterloo.ca>
2020-10-13 01:12:09 -0400

    Changes

--- gibele/hosts.py
+++ gibele/hosts.py
@@ -3,9 +3,10 @@ from django_hosts import patterns, host
 
 settings.PARENT_HOST = 'gibele.com:8000'
 host_patterns = patterns('',
-    host(r'www', settings.ROOT_URLCONF, name='www'),
     # host(r'www', 'gibele.urls',name='ww'),
     host(r'business','businessadmin.urls', name='bizadmin'),
     host(r'(?P<slug>[-\w]+)', 'calendarapp.urls', callback='calendarapp.views.get_companyslug',name='bookingurl'),
+
+    host(r'', settings.ROOT_URLCONF, name='www'),
     # host(r'', settings.ROOT_URLCONF, name='www'),
 )

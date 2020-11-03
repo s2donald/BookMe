a46e0e4ab4dec031a06fd5a3419be75602b00a5a
Stephen Donald <s2donald@uwaterloo.ca>
2020-11-03 01:25:09 -0500

    Heroku deployment

--- calendarapp/models.py
+++ calendarapp/models.py
@@ -4,7 +4,7 @@ from consumer.models import Bookings
 # Create your models here.
 
 class formBuilder(models.Model):
-    booking = models.ForeignKey(Bookings, on_delete=models.CASCADE, related_name='form')
+    booking = models.ForeignKey(Bookings, on_delete=models.CASCADE, related_name='form', null=True)
     label = models.CharField(max_length=50, db_index=True)
     checkbox = models.BooleanField(null=True, default=False)
     text = models.TextField(max_length=100, null=True)

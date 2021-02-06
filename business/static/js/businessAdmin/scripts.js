6abef202a307999e305c42bcfd8e3741175daead
Stephen Donald <s2donald@uwaterloo.ca>
2021-02-05 22:31:40 -0500

    Added changes to the form fields input

--- business/static/js/businessAdmin/scripts.js
+++ business/static/js/businessAdmin/scripts.js
@@ -263,10 +263,11 @@ $(document).ready(function() {
 
   // Activate Popovers and set color for popovers
   $('[data-toggle="popover"]').each(function() {
-    color_class = $(this).data('color');
-    $(this).popover({
-      template: '<div class="popover popover-' + color_class + '" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
-    });
+    // console.log($(this))
+    // color_class = $(this).data('color');
+    // $(this).popover({
+    //   template: '<div class="popover popover-' + color_class + '" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
+    // });
   });
 
   var tagClass = $tagsinput.data('color');
